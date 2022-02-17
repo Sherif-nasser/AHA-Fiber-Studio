2c1f58e7d857b37370569abe2a913ae44b036e2d
sherif naser <s.naser@Bolts.com>
2022-02-17 02:39:02 +0200

    the first edit in the landing page

--- src/Layouts/nav-bar/nav-bar.component.ts
+++ src/Layouts/nav-bar/nav-bar.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
+
+
+
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  navbarfixed:boolean = false;
+
+  constructor() {}
+
+ @ViewChild("headerElement") headerElement: ElementRef;
+
+  @HostListener('window:scroll', ['$event']) onScroll(){
+
+      if(window.scrollY > this.headerElement.nativeElement.offsetTop){
+        this.navbarfixed = true;
+     
+        }else{
+          this.navbarfixed=false;
+        
+        }
+  }
 
   ngOnInit(): void {
+
   }
 
 }
+
+// window.onscroll = function() {myFunction()};
+
+
+// var sticky = navbar.offsetTop;
+
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
+
+
+//function buildName(firstName: string, ...restOfName: string[]) {
+//   return firstName + " " + restOfName.join(" ");
+// }
